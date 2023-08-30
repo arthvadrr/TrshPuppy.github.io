d171b6a913234f1c871a96f357cda5b8b6725ebe
Rose <trshpuppy@proton.me>
2023-08-30 18:22:39 +0100

    about section can serve new index.html file

--- main.go
+++ main.go
@@ -31,8 +31,6 @@ func get_about_content() string {
 func indexHandler(respWriter http.ResponseWriter, req *http.Request){
 	// Create template from parsed index.html:
 	var tpl = template.Must(template.ParseFiles("index.html"))
-
-	// respWriter.Write([]byte("<h1>Hello World!</h1>"))
 	tpl.Execute(respWriter, nil)
 }
 
@@ -55,14 +53,6 @@ func aboutHandler(respWriter http.ResponseWriter, req *http.Request){
 	// get the new content for index.html:
 	about_html := get_about_content()
 
-	// modify index.html
-	// modify_index := modify_index_html() // this should return an HTML file/ change the index.html file in root dir
-	// index_file, err := os.OpenFile("index.html", os.O_RDWR, 0666) // filemode == linux perms??
-	// if err != nil {
-	// 	fmt.Printf("ERROR: line 40 :)")
-	// 	return
-	// }
-
 	index_file, err := os.OpenFile("index.html", os.O_RDWR, 0666) // filemode == linux perms??
 	if err != nil {
 		fmt.Printf("ERROR: line 67 :)")
@@ -81,58 +71,10 @@ func aboutHandler(respWriter http.ResponseWriter, req *http.Request){
 	// Close file:
 	index_file.Close()
 
-	// HERE
-
-	// if assign; expr { error_handling },
-
-
-	// // ** file.WriteAt <-- writes starting at a specific length of bytes into file (need length offset)
-	// // ** find byte offset:   file.Read()?  bufio.newScanner() <-- line by line
-	// offset_line := "  <body>" // <-- probably should be something like "includes '<body" instead
-	// scanner := bufio.NewScanner(index_file)
-	// bytes_offset := len(offset_line)
-
-	// // loop through file line by line to find offset byte:
-	// for scanner.Scan(){
-	// 	// Read current line & save line number:
-	// 	current_line_txt := scanner.Text()
-		
-	// 	fmt.Printf(current_line_txt)
-	// 	bytes_offset += len(scanner.Bytes())
-	// 	// Compare line to offset_line
-	// 	if current_line_txt == offset_line{
-	// 		// Get # bytes into file that offset pattern happens
-	// 		fmt.Printf("wow biotch, u did it, the line is %s", len(scanner.Bytes()))
-	// 		//byte_offset = len(scanner.Bytes())
-	// 		break
-	// 	}
-	// }
-	// fmt.Printf("byte offset var ", bytes_offset)
-
-	// defer index_file.Close()
-
-	// Use byte offset to set where we edit file...
-	// about_html = "    " + about_html
-	// content_length := len(about_html)
-	// if _, err := index_file.WriteAt([]byte(about_html), int64(byte_offset)); err != nil{
-	// 	fmt.Printf("ERROR: line 70 :) +", err)
-	// 	return
-	// }
-
-	// index_file.WriteAt([]byte("\n" + about_html), int64(bytes_offset));
-
-	// // Close file:
-	// index_file.Close()
-
-
-
-
-
-	// // Create new template:
-	// var about_tpl = template.Must(template.ParseFiles("index.html"))
-
-	// // Execute new template:
-	// about_tpl.Execute(respWriter, nil)
+	// Serve new index.html
+	// Create template from parsed index.html:
+	var tpl = template.Must(template.ParseFiles("index.html"))
+	tpl.Execute(respWriter, nil)
 }
 
 
